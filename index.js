484689221410e0a8bf80e19723caf9d8decf821f
latinawhocodes <alyssaroque01@gmail.com>
2019-10-27 11:37:37 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(team){
+  let result = team.find(function(result) {
+    return result.result == "W"
+   })
+   if (result){
+     return result.year
+   }
+}
